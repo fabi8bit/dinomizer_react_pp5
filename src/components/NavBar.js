72a86b25a29c188b85c0dc93ec8f2dddd9741b91
fabi8bit <fabi8bit@gmail.com>
2023-10-01 17:25:51 +0000

    Add SignInForm.js
    Implement context for NavBar to determine what is visible if logged in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,76 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "../styles/NavBar.module.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/dm-logo-navbar.svg";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import LoginIcon from "@mui/icons-material/Login";
 import { NavLink } from "react-router-dom/cjs/react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        exact
+        to="/signin"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <LoginIcon className={styles.Icon} />
+        Sign in
+      </NavLink>
+      <NavLink
+        exact
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <PersonAddIcon className={styles.Icon} />
+        Sign up
+      </NavLink>
+    </>
+  );
+
+  const loggedInIcons = (
+    <>
+      <NavLink
+        exact
+        to="/signin"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <LoginIcon className={styles.Icon} />
+        Timeline
+      </NavLink>
+      <NavLink
+        exact
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <PersonAddIcon className={styles.Icon} />
+        projects
+      </NavLink>
+      <NavLink
+        exact
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <PersonAddIcon className={styles.Icon} />
+        Contribute
+      </NavLink>
+      {currentUser?.username}
+    </>
+  );
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <Navbar.Brand>
           <img
@@ -20,14 +82,7 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink exact to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
-              <LoginIcon className={styles.Icon} />
-              Sign in
-            </NavLink>
-            <NavLink exact to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
-              <PersonAddIcon className={styles.Icon} />
-              Sign up
-            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,108 @@
+import React, { useContext, useState} from "react";
+import { Alert, Button, Col, Container, Form, Image, Row } from "react-bootstrap";
+import logo from "../../assets/dm-logo-orig-white-txt.png";
+import styles from "../../styles/SignInUpForm.module.css"
+import axios from "axios";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import { SetCurrentUserContext } from "../../App";
+
+
+const SignInForm = () => {
+  const setCurrentUser = useContext(SetCurrentUserContext);
+
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const {data} = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Row className={`${styles.Row} d-flex align-items-center`} >
+      <Col lg={6} sm={12}>
+        <Container>
+        <h1 className={styles.Header}>Sign in</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">username</Form.Label>
+              <Form.Control
+                size="lg"
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="danger" key={idx}>
+                Username - {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                size="lg"
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert variant="danger" key={idx}>
+                Password - {message}
+              </Alert>
+            ))}
+
+            <Button
+              variant="outline-warning"
+              type="submit"
+            >
+              Sign in
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="danger" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+      </Col>
+      <Col>
+        <Container>
+          <Image
+          className={styles.FillerImage}
+            src={logo}
+          />
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignInForm;

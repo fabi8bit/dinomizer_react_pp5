e27ab375dcecdd1ad424910d6eaeeca374f401bb
fabi8bit <fabi8bit@gmail.com>
2023-10-02 10:22:26 +0000

    Fix url axios.post for axiosReq and axiosRes in CurrentUserContext

--- src/context/CurrentUserContext.js
+++ src/context/CurrentUserContext.js
@@ -31,29 +31,29 @@ export const CurrentUserProvider = ({ children }) => {
     axiosReq.interceptors.request.use(
       async (config) => {
         try {
-          await axios.post('dj-rest-auth/token/refresh/')
-        } catch(err) {
+          await axios.post("/dj-rest-auth/token/refresh/");
+        } catch (err) {
           setCurrentUser((prevCurrentUser) => {
             if (prevCurrentUser) {
-              history.push('/signin')
+              history.push("/signin");
             }
-            return null
-          })
-          return (config)
+            return null;
+          });
+          return config;
         }
-        return (config)
+        return config;
       },
       (err) => {
         return Promise.reject(err);
       }
-    )
+    );
 
     axiosRes.interceptors.response.use(
       (response) => response,
       async (err) => {
         if (err.response?.status === 401) {
           try {
-            await axios.post("dj-rest-auth/token/refresh/");
+            await axios.post("/dj-rest-auth/token/refresh/");
           } catch (err) {
             setCurrentUser((prevCurrentUser) => {
               if (prevCurrentUser) {
@@ -67,7 +67,7 @@ export const CurrentUserProvider = ({ children }) => {
         return Promise.reject(err);
       }
     );
-  },[history]);
+  }, [history]);
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
